27e59e597c391891f306011297a7025152660740
shakadak <nathanael.pineau@gmail.com>
2015-05-14 19:00:28 +0200

    prototypes

--- inc/libmtx.h
+++ inc/libmtx.h
@@ -17,9 +17,11 @@ typedef struct	s_mtx
 }				t_mtx;
 
 t_mtx			mtx_add(t_mtx const lh, t_mtx const rh);
+double			mtx_determinant(t_mtx const mtx);
 double			mtx_dot(t_mtx const lh, unsigned int const i, t_mtx const rh,
 		unsigned int j);
 t_mtx			mtx_identity(unsigned int const size);
+double			mtx_minor(t_mtx const mtx, int const row, int const col);
 t_mtx			mtx_product(t_mtx const lh, t_mtx const rh);
 t_mtx			mtx_scalar_mult(double const lambda, t_mtx const mtx);
 t_mtx			mtx_sub(t_mtx const lh, t_mtx const rh);
